ba3a80532c5984a1030860323dfd99a544abb041
Karen Matevosyan <mr.mgo@yandex.ru>
2021-09-04 20:46:01 +0400

    Add test for count sort

--- src/main/java/com/dataStructures/sort/countSort/CountSort.java
+++ src/main/java/com/dataStructures/sort/countSort/CountSort.java
@@ -4,25 +4,29 @@ import java.util.Arrays;
 
 public class CountSort {
     public static void main(String[] args) {
+        CountSort cs = new CountSort();
         int[] arr = {24, 34, 84, 45, 13, 45, 68, 69, 41, 13, 47, 89};
-        countSort(arr);
+        cs.sort(arr, 100);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void countSort(int[] arr) {
-        final int MAX_VALUE = 100;
-        int[] count = new int[MAX_VALUE];
+    public int[] sort(int[] arr, int maxValue) {
+        int[] arrClone = arr.clone();
 
-        for (int k : arr) {
+        int[] count = new int[maxValue];
+
+        for (int k : arrClone) {
             count[k]++;
         }
 
         int arrayIndex = 0;
         for (int i = 0; i < count.length; i++) {
             for (int j = 0; j < count[i]; j++) {
-                arr[arrayIndex] = i;
+                arrClone[arrayIndex] = i;
                 arrayIndex++;
             }
         }
+
+        return arrClone;
     }
 }
---
+++ src/test/java/com/dataStructures/sort/countSort/CountSortTest.java
@@ -0,0 +1,223 @@
+package com.dataStructures.sort.countSort;
+
+import com.dataStructures.sort.choiceSort.ChoiceSort;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+public class CountSortTest {
+
+    private static CountSort cs;
+
+    @BeforeClass
+    public static void setClass() {
+        cs = new CountSort();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 10);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_100_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 100);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_1000_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 1000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_000_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 10_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_100_000_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 100_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_1_000_000_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 1_000_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_000_000_elements_100_max_value() {
+        int maxValue = 100;
+        int[] data = getArray(0, maxValue, 10_000_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 10);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_100_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 100);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_1000_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 1000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_000_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 10_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_100_000_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 100_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_1_000_000_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 1_000_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_000_000_elements_1000_max_value() {
+        int maxValue = 1000;
+        int[] data = getArray(0, maxValue, 10_000_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 10);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_100_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 100);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_1000_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 1000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_000_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 10_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_100_000_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 100_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_1_000_000_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 1_000_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    @Test(timeout = 10_000)
+    public void sort_10_000_000_elements_10000_max_value() {
+        int maxValue = 10000;
+        int[] data = getArray(0, maxValue, 10_000_000);
+        int[] sortData = cs.sort(data, maxValue);
+        assertThat(sortData).isNotEqualTo(data)
+                .isSorted();
+    }
+
+    private int[] getArray(int min, int max, int length) {
+        int[] data = new int[length];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = random(min, max);
+        }
+        return data;
+    }
+
+    private int random(int min, int max) {
+        return min + (int) (Math.random() * max);
+    }
+}
